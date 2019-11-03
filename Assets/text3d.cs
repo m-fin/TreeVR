69c8d2c6b073d185b9408535662c09dd0fa13791
Matthew Finley <mattjf67@gmail.com>
2019-11-03 11:35:27 -0500

    g

---
+++ Assets/text3d.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class text3d : MonoBehaviour {
+    TextMesh text;
+
+    float startTime;
+    int empathyScore;
+
+    // Use this for initialization
+    void Start () {
+        text= gameObject.GetComponent("TextMesh") as TextMesh;
+
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        empathyScore = (int)(Time.time - startTime);
+
+        text.text = "Empathy Score: " + empathyScore.ToString();
+    }
+}
